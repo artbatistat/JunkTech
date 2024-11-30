55b340fce1f78c239cfdec6211cee190d338ee54
Emanuel <emanuelnevesti@gmail.com>
2024-11-30 16:35:34 -0300

    Inserindo localização do ponto

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,99 @@
+import { GoogleMap, LoadScript, Marker, StandaloneSearchBox } from "@react-google-maps/api";
+import React, { useContext } from "react";
+import { AuthEmailContext } from "../../contexts/authEmail";
+import PageFooter from "../../layout/PageFooter";
+import PageNavegation from "../../layout/PageNavegation";
+import './map.css';
+
+
+export const Profile = () => {
+
+   async  function setPosition(position){
+    console.log(position)
+    console.log(user)
+        // var data = JSON.stringify({
+        //     email: email,
+        //     password: pwd,
+        //     user_type: userType
+        //   })
+
+        //   const response = await fetch('http://cors-anywhere.herokuapp.com/http://junktech.vercel.app/pickup-point', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     },
+        //     body: data
+        //   });
+    }
+
+    const position = { lat: -19.957054, lng: -44.034900 }
+    const [map, setMap] = React.useState(undefined);
+    const [markers, setMarkers] = React.useState([])
+
+    const onMapLoad = (map) => {
+        setMap(map);
+      };
+
+    const [searchBox, setSeatchBox] = React.useState(null);
+
+    const onLoad = (ref) => {
+        setSeatchBox(ref)
+    }
+
+    const onPlacesChanged = () => {
+        const places = searchBox.getPlaces();
+        const place = places[0];
+        const location = {
+            lat: place?.geometry?.location?.lat() || 0,
+            lng: place?.geometry?.location?.lng() || 0
+        }
+
+        setPosition(location)
+
+        map.panTo(location)
+        setMarkers([...markers, location])
+    }
+
+    const { user } = useContext(AuthEmailContext)
+
+    if (!user) {
+        return '';
+    }
+
+    return (
+        <>
+            <PageNavegation></PageNavegation>
+
+            < br /> <h1>Profile Page </h1>
+            < h3 > Bem - vindo {user.username} </h3>
+
+            < div className="map" >
+                <LoadScript
+                    id="script-loader"
+                    googleMapsApiKey="AIzaSyAEY2Hu2axCPl8IVCPz9gmglcaK_jGDUW8"
+                    libraries={['places']}
+                >
+
+                    <GoogleMap
+                        onLoad={onMapLoad}
+                        mapContainerStyle={{ width: '100%', height: '100%' }
+                        }
+                        center={position}
+                        zoom={15}
+                    >
+
+                        <StandaloneSearchBox onLoad={onLoad} onPlacesChanged={onPlacesChanged}>
+                            <input className="address" placeholder="Digite um endereço" type="text"/>
+
+                        </StandaloneSearchBox>
+
+                        {markers.map((marker, index) => (
+                            <Marker key={index} position={marker} />
+                        ))}
+                    </GoogleMap>
+                </LoadScript>
+            </div>
+            <PageFooter></PageFooter>
+        </>
+    )
+}
